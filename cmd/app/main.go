ced4f3ff8b8dcd67e417762ce6b2f70d28ef0235
alireza khaki <a.mansourkhaki@domil.io>
2023-06-15 21:58:19 +0330

    3: add user repo

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"log"
 
+	// "github.com/google/uuid"
 	"gitlab.avakatan.ir/boilerplates/go-boiler/config"
 	"gitlab.avakatan.ir/boilerplates/go-boiler/internal/infrastructure/database/migration/handler"
+	// command_model "gitlab.avakatan.ir/boilerplates/go-boiler/internal/infrastructure/database/model/command"
 	"gitlab.avakatan.ir/boilerplates/go-boiler/internal/infrastructure/database/persistence"
-	repository "gitlab.avakatan.ir/boilerplates/go-boiler/internal/infrastructure/database/repository/cache"
+	repository "gitlab.avakatan.ir/boilerplates/go-boiler/internal/infrastructure/database/repository"
 )
 
 var DbConnection *sql.DB
@@ -18,6 +20,8 @@ func main() {
 	configData := config.ProvideConfig()
 	db, _ := persistence.NewSqlDatabaseConn("postgres", configData.PostgresDb)
 	defer db.Close()
+	TestUserRepo(db)
+
 	if err := migration.RunMigration(db); err != nil {
 		log.Fatalf("failed to run migrations: %v", err)
 	}
@@ -35,4 +39,41 @@ func main() {
 
 	redisRepo.Set("hello", "hello world!")
 	fmt.Println(redisRepo.Get("hello"))
+}
+
+func TestUserRepo(db *persistence.Database) {
+	// Create an instance of the SQLUserRepository
+	// userRepo := repository.NewSQLUserRepository(db)
+
+	// // Create a new user
+	// user := &command_model.User{
+	// 	ID:       uuid.New(),
+	// 	Username: "john_doe",
+	// 	Email:    "john@example.com",
+	// 	Password: "secret",
+	// 	PersonID: uuid.New(),
+	// }
+	// err := userRepo.Create(user)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// // Retrieve a user by ID
+	// retrievedUser, err := userRepo.GetByID(user.ID)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// // Update the user
+	// user.Username = "jdoe"
+	// err = userRepo.Update(user)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// // Delete the user
+	// err = userRepo.Delete(user.ID)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 }
