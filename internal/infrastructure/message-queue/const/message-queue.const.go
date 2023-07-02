db74e84b3590d221a1915d28e910f77858edfe3a
Kiarash Salehi <k.salehi@domil.io>
2023-07-02 12:00:18 +0330

    7: clean up message queue

---
+++ internal/infrastructure/message-queue/const/message-queue.const.go
@@ -0,0 +1,8 @@
+package queueconst
+
+const (
+	DefaultQueue     = "default"
+	FirstEmailQueue  = "first-email"
+	SecondEmailQueue = "second-email"
+	ImageResizeQueue = "image"
+)
--- internal/infrastructure/message-queue/message-queue.go
+++ internal/infrastructure/message-queue/message-queue.go
@@ -2,61 +2,55 @@ package messagequeue
 
 import (
 	"encoding/json"
+	"fmt"
 	"time"
 
 	"github.com/hibiken/asynq"
 	errorhandler "gitlab.avakatan.ir/boilerplates/go-boiler/internal/infrastructure/error-handler"
 	"gitlab.avakatan.ir/boilerplates/go-boiler/internal/infrastructure/logging"
+	queueconst "gitlab.avakatan.ir/boilerplates/go-boiler/internal/infrastructure/message-queue/const"
 	queuehandler "gitlab.avakatan.ir/boilerplates/go-boiler/internal/infrastructure/message-queue/handler"
 	queuemiddleware "gitlab.avakatan.ir/boilerplates/go-boiler/internal/infrastructure/message-queue/middleware"
 	"gitlab.avakatan.ir/boilerplates/go-boiler/internal/infrastructure/task"
 )
 
-// A list of channels.
-const (
-	EmailChannel       = "email"
-	ImageResizeChannel = "image"
-	TestChannel        = "test"
-)
-
 type AsynqMQ struct {
-	RedisAddr string
-	Client    *asynq.Client
+	client    *asynq.Client
 	Scheduler *asynq.Scheduler
+	Inspector *asynq.Inspector
 }
 
 func (mq *AsynqMQ) Enqueue(t *asynq.Task, queue string, opts ...asynq.Option) (*asynq.TaskInfo, error) {
 	opts = append(opts, asynq.Queue(queue))
-	info, err := mq.Client.Enqueue(t, opts...)
-	logging.Logger.Info().Msgf("enqueued task: id=%s queue=%s", info.ID, info.Queue)
+	info, err := mq.client.Enqueue(t, opts...)
 	return info, err
 }
 
-func (mq *AsynqMQ) Init() {
-	mq.InitClient()
-	mq.InitScheduler(asynq.RedisClientOpt{Addr: mq.RedisAddr}, &asynq.SchedulerOpts{Logger: &logging.AsynqZerologLogger{Logger: logging.Logger}})
-}
-
-func (mq *AsynqMQ) InitClient() {
-	client := asynq.NewClient(asynq.RedisClientOpt{Addr: mq.RedisAddr})
-	mq.Client = client
-}
-
-func (mq *AsynqMQ) InitScheduler(redisOpt asynq.RedisClientOpt, schedulerOpts *asynq.SchedulerOpts) {
-	scheduler := asynq.NewScheduler(redisOpt, schedulerOpts)
-	mq.Scheduler = scheduler
+func (mq *AsynqMQ) PushToOtherQueue(sourceQueue string, destinationQueue string) error {
+	tasks, err := mq.Inspector.ListPendingTasks(sourceQueue, asynq.PageSize(-1))
+	if err != nil {
+		return fmt.Errorf("failed to fetch tasks from source queue: %v", err)
+	}
+	for _, task := range tasks {
+		_, err := mq.client.Enqueue(asynq.NewTask(task.Type, task.Payload), asynq.Queue(destinationQueue))
+		if err != nil {
+			return fmt.Errorf("failed to push task to destination queue: %v", err)
+		} else {
+			mq.Inspector.DeleteTask(sourceQueue, task.ID)
+		}
+	}
+	return nil
 }
 
 func NewAsynqMQ(redisAddr string) *AsynqMQ {
+	client := asynq.NewClient(asynq.RedisClientOpt{Addr: redisAddr})
+	scheduler := asynq.NewScheduler(asynq.RedisClientOpt{Addr: redisAddr}, &asynq.SchedulerOpts{})
+	inspector := asynq.NewInspector(asynq.RedisClientOpt{Addr: redisAddr})
 	mq := &AsynqMQ{
-		RedisAddr: redisAddr,
+		client:    client,
+		Scheduler: scheduler,
+		Inspector: inspector,
 	}
-	mq.Init()
-	return mq
-}
-
-func InitMessageQueue(redisAddr string) *AsynqMQ {
-	mq := NewAsynqMQ(redisAddr)
 	go func() {
 		err := mq.Scheduler.Run()
 		errorhandler.CheckForError("Could not run schedulers: %v", err, errorhandler.TErrorData{})
@@ -64,15 +58,16 @@ func InitMessageQueue(redisAddr string) *AsynqMQ {
 	return mq
 }
 
-func InitMessageQueueMuxServer(redisAddr string) {
+func ProcessQueues(redisAddr string) {
 	srv := asynq.NewServer(
 		asynq.RedisClientOpt{Addr: redisAddr},
 		asynq.Config{
 			Concurrency: 10,
 			Queues: map[string]int{
-				"default":          1,
-				EmailChannel:       2,
-				ImageResizeChannel: 2,
+				queueconst.DefaultQueue:     1,
+				queueconst.FirstEmailQueue:  2,
+				queueconst.SecondEmailQueue: 2,
+				queueconst.ImageResizeQueue: 2,
 			},
 			Logger: &logging.AsynqZerologLogger{
 				Logger: logging.Logger,
@@ -90,14 +85,18 @@ func InitMessageQueueMuxServer(redisAddr string) {
 }
 
 func TestMessageQueue(redisAddr string) *AsynqMQ {
-	mq := InitMessageQueue(redisAddr)
+	mq := NewAsynqMQ(redisAddr)
 	t, err := task.NewEmailDeliveryTask(42, "some-template-id")
 	errorhandler.CheckForError("Could not enqueue email: %v", err, errorhandler.TErrorData{})
-	_, err = mq.Enqueue(t, EmailChannel, asynq.Retention(2*time.Minute))
+	_, err = mq.Enqueue(t, queueconst.FirstEmailQueue, asynq.Retention(2*time.Minute))
 	errorhandler.CheckForError("Could not enqueue image resize: %v", err, errorhandler.TErrorData{})
 	emailPayload, _ := json.Marshal(task.EmailDeliveryPayload{UserID: 1, TemplateID: "interval-temp"})
-	mq.Scheduler.Register("@every 30s", asynq.NewTask(task.EmailDeliveryTask, emailPayload, asynq.Queue(EmailChannel)))
+	mq.Scheduler.Register("@every 2s", asynq.NewTask(task.EmailDeliveryTask, emailPayload, asynq.Queue(queueconst.FirstEmailQueue)))
+	mq.PushToOtherQueue(queueconst.SecondEmailQueue, queueconst.FirstEmailQueue)
 
-	InitMessageQueueMuxServer(redisAddr)
+	firstEmailQueueInfo, _ := mq.Inspector.GetQueueInfo(queueconst.FirstEmailQueue)
+	logging.Logger.Info().Interface("firstEmailQueueInfo", firstEmailQueueInfo).Msg("")
+
+	ProcessQueues(redisAddr)
 	return mq
 }
