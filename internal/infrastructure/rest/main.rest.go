169d08999ecf0b618055cc71ef172c3fa5fa32a5
Kiarash-Salehi <skiarash82@gmail.com>
2023-07-28 20:00:20 +0330

    14: use logger to log request info

--- internal/infrastructure/rest/main.rest.go
+++ internal/infrastructure/rest/main.rest.go
@@ -3,15 +3,29 @@ package rest
 import (
 	"github.com/gin-gonic/gin"
 	constants "gitlab.avakatan.ir/boilerplates/go-boiler/internal/infrastructure/constant"
+	"gitlab.avakatan.ir/boilerplates/go-boiler/internal/infrastructure/logging"
 )
 
 func SetupRouter(appEnv string) *gin.Engine {
 	if appEnv == constants.PRODUCTION {
 		gin.SetMode(gin.ReleaseMode)
 	}
-	gin.Logger()
-	router := gin.Default()
-	// router.Use(gin.LoggerWithFormatter(logging.AppLogger))
-	// Add your middleware here (if needed)
+	router := gin.New()
+	router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		logging.Info(logging.LoggerInput{
+			Data: map[string]any{
+				"ClientIP":     param.ClientIP,
+				"Method":       param.Method,
+				"Path":         param.Path,
+				"StatusCode":   param.StatusCode,
+				"Latency":      param.Latency,
+				"ErrorMessage": param.ErrorMessage,
+				"Body":         param.Request.Body,
+				"Header":       param.Request.Header,
+			},
+		})
+		return ""
+	}))
+	router.Use(gin.Recovery())
 	return router
 }
