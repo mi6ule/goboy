2ad7cfbb47ea304338a4b0111f087db29fe09d0f
Kiarash-Salehi <skiarash82@gmail.com>
2023-08-30 11:54:13 +0330

    17: move db tests to their own file

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"sync"
 
 	"net"
 
@@ -11,10 +10,9 @@ import (
 	userpb "gitlab.avakatan.ir/boilerplates/go-boiler/gen/go/proto/user/v1"
 	constants "gitlab.avakatan.ir/boilerplates/go-boiler/internal/infrastructure/constant"
 	migration "gitlab.avakatan.ir/boilerplates/go-boiler/internal/infrastructure/database/migration/handler"
-	query_model "gitlab.avakatan.ir/boilerplates/go-boiler/internal/infrastructure/database/model/query"
 	"gitlab.avakatan.ir/boilerplates/go-boiler/internal/infrastructure/database/persistence"
 	cacheRepository "gitlab.avakatan.ir/boilerplates/go-boiler/internal/infrastructure/database/repository/cache"
-	readRepository "gitlab.avakatan.ir/boilerplates/go-boiler/internal/infrastructure/database/repository/query"
+	dbtest "gitlab.avakatan.ir/boilerplates/go-boiler/internal/infrastructure/database/test"
 	"gitlab.avakatan.ir/boilerplates/go-boiler/internal/infrastructure/elastic"
 	errorhandler "gitlab.avakatan.ir/boilerplates/go-boiler/internal/infrastructure/error-handler"
 
@@ -65,7 +63,7 @@ func main() {
 	err = elastic.TestElastic(client)
 	errorhandler.ErrorHandler(errorhandler.ErrorInput{Err: err})
 
-	TestClientRepo(mongoClient, redisClient)
+	dbtest.TestClientRepo(mongoClient, redisClient)
 
 	// Run gRPC server
 	go startGRPCServer(mongoClient, redisClient)
@@ -88,90 +86,4 @@ func startGRPCServer(db *persistence.MongoDatabase, redisClient *persistence.Red
 	grpcServer := grpc.NewServer()
 	userpb.RegisterUserServiceServer(grpcServer, grpcUserService)
 	grpcServer.Serve(lis)
-}
-
-func TestClientRepo(db *persistence.MongoDatabase, redisClient *persistence.RedisClient) {
-	clientRepository := readRepository.NewMongoDBClientRepository(db.Database, redisClient)
-
-	client := &query_model.Client{
-		ID: 123456789,
-		// UserID:    123456789,
-		// PersonID:  123456798,
-		// FirstName: "alireza",
-		// LastName:  "khaki",
-		// Age:       25,
-		// Username:  "a.khaki",
-		// Email:     "a.khaki@domil.io",
-		// Password:  "pass",
-	}
-
-	// Use a channel to receive the findClient value
-	findClientChan := make(chan *query_model.Client, 1)
-
-	// Use a WaitGroup to wait for all goroutines to finish
-	var wg sync.WaitGroup
-
-	// Increment the WaitGroup counter
-	wg.Add(1)
-
-	err := clientRepository.Create(client)
-	errorhandler.ErrorHandler(errorhandler.ErrorInput{Err: err, ErrType: "Fatal", Code: constants.ERROR_CODE_100006})
-
-	// Use goroutine for the GetByID operation
-	go func() {
-		defer wg.Done()
-
-		findClient, err := clientRepository.GetByID(123456789)
-		errorhandler.ErrorHandler(errorhandler.ErrorInput{Err: err, ErrType: "Fatal", Code: constants.ERROR_CODE_100007})
-
-		findClientChan <- findClient // Send the findClient value through the channel
-	}()
-
-	// Wait for goroutine to finish
-	wg.Wait()
-
-	// Close the channel to signal that no more values will be sent
-	close(findClientChan)
-
-	// Receive the findClient value from the channel
-	findClient, ok := <-findClientChan
-	if !ok {
-		// Handle the case where findClient value is not received
-		logging.Info(logging.LoggerInput{Message: "findClientChan has no return value"})
-	}
-
-	// Use the findClient variable
-	if findClient != nil {
-		logging.Info(logging.LoggerInput{Message: fmt.Sprintf("findClient: %v", *findClient)})
-	} else {
-		// Handle the case where findClient is nil
-		logging.Info(logging.LoggerInput{Message: "client not found"})
-	}
-	logging.Info((logging.LoggerInput{Message: fmt.Sprintf("Found client's age is: %v", findClient.Age)}))
-}
-
-func TestUserRepo(db *persistence.Database) {
-	// Create an instance of the SQLUserRepository
-	// userRepo := repository.NewSQLUserRepository(db)
-
-	// // Create a new user
-	// user := &command_model.User{
-	// 	ID:       uuid.New(),
-	// 	Username: "john_doe",
-	// 	Email:    "john@example.com",
-	// 	Password: "secret",
-	// 	PersonID: uuid.New(),
-	// }
-	// err := userRepo.Create(user)
-	// 	errorhandler.ErrorHandler(err, errorhandler.TErrorData{"errType":"Fatal"}).Err(err).Msg("")
-	// // Retrieve a user by ID
-	// retrievedUser, err := userRepo.GetByID(user.ID)
-	// 	errorhandler.ErrorHandler(err, errorhandler.TErrorData{"errType":"Fatal"}).Err(err).Msg("")
-	// // Update the user
-	// user.Username = "jdoe"
-	// err = userRepo.Update(user)
-	// 	errorhandler.ErrorHandler(err, errorhandler.TErrorData{"errType":"Fatal"}).Err(err).Msg("")
-	// // Delete the user
-	// err = userRepo.Delete(user.ID)
-	// 	errorhandler.ErrorHandler(err, errorhandler.TErrorData{"errType":"Fatal"}).Err(err).Msg("")
 }
