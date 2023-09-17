feabac9439363a53a5531b7e39219755bab63161
Kiarash <k.salehi@domil.io>
2023-09-17 12:29:15 +0330

    18: use error code constants in error message map

--- internal/infrastructure/constant/error.const.go
+++ internal/infrastructure/constant/error.const.go
@@ -2,7 +2,10 @@ package constants
 
 const (
 	ERROR_CODE_100001 = "100001"
+	ERROR_CODE_100002 = "100002"
+	ERROR_CODE_100003 = "100003"
 	ERROR_CODE_100004 = "100004"
+	ERROR_CODE_100005 = "100005"
 	ERROR_CODE_100006 = "100006"
 	ERROR_CODE_100007 = "100007"
 	ERROR_CODE_100008 = "100008"
@@ -17,11 +20,10 @@ const (
 	ERROR_CODE_100017 = "100017"
 	ERROR_CODE_100018 = "100018"
 	ERROR_CODE_100019 = "100019"
-
 	ERROR_CODE_100020 = "100020"
 	ERROR_CODE_100021 = "100021"
 	ERROR_CODE_100022 = "100022"
-	ERROR_CODE_100023 = "100035"
+	ERROR_CODE_100023 = "100023"
 	ERROR_CODE_100024 = "100024"
 	ERROR_CODE_100025 = "100025"
 	ERROR_CODE_100026 = "100026"
@@ -37,35 +39,36 @@ const (
 )
 
 var ErrorMessage = map[string]string{
-	"100001":          "failed to import env variables",
-	"100002":          "failed to run migrations",
-	"100003":          "failed to connect to mongoDb",
-	"100004":          "failed to connect to redis",
-	"100005":          "",
-	"100006":          "",
-	"100007":          "",
-	"100008":          "invalid connection string",
-	"100009":          "failed to connect to mongodb",
-	"100010":          "failed to connect to mongodb",
-	"100011":          "",
-	"100012":          "",
-	"100013":          "failed to run asynq scheduler",
-	"100014":          "failed to init mux server",
-	"100015":          "failed to enqueue email",
-	"100016":          "failed to enqueue image resize",
-	"100017":          "failed to connect to postgresql db",
-	"100018":          "failed to listen gin server",
-	"100019":          "failed to create elastic client",
-	"100021":          "failed to create admin client",
-	"100022":          "failed to create topics",
-	"100023":          "KAFKA: Error in creating producer",
-	"100024":          "KAFKA: Error in creating producer",
-	"100025":          "Error in person producer",
-	"100026":          "delivery failed",
-	"100027":          "KAFKA: Error in subscribing to the topic",
-	"100028":          "KAFKA: Error in fetch messages from the topic",
-	"100029":          "",
-	"100030":          "",
+	ERROR_CODE_100001: "failed to import env variables",
+	ERROR_CODE_100002: "failed to run migrations",
+	ERROR_CODE_100003: "failed to connect to mongoDb",
+	ERROR_CODE_100004: "failed to connect to redis",
+	ERROR_CODE_100005: "",
+	ERROR_CODE_100006: "",
+	ERROR_CODE_100007: "",
+	ERROR_CODE_100008: "invalid connection string",
+	ERROR_CODE_100009: "failed to connect to mongodb",
+	ERROR_CODE_100010: "failed to connect to mongodb",
+	ERROR_CODE_100011: "",
+	ERROR_CODE_100012: "",
+	ERROR_CODE_100013: "failed to run asynq scheduler",
+	ERROR_CODE_100014: "failed to init mux server",
+	ERROR_CODE_100015: "failed to enqueue email",
+	ERROR_CODE_100016: "failed to enqueue image resize",
+	ERROR_CODE_100017: "failed to connect to postgresql db",
+	ERROR_CODE_100018: "failed to listen gin server",
+	ERROR_CODE_100019: "failed to create elastic client",
+	ERROR_CODE_100020: "",
+	ERROR_CODE_100021: "failed to create admin client",
+	ERROR_CODE_100022: "failed to create topics",
+	ERROR_CODE_100023: "KAFKA: Error in creating producer",
+	ERROR_CODE_100024: "KAFKA: Error in creating producer",
+	ERROR_CODE_100025: "Error in person producer",
+	ERROR_CODE_100026: "delivery failed",
+	ERROR_CODE_100027: "KAFKA: Error in subscribing to the topic",
+	ERROR_CODE_100028: "KAFKA: Error in fetch messages from the topic",
+	ERROR_CODE_100029: "",
+	ERROR_CODE_100030: "",
 	ERROR_CODE_100031: "failed to enqueue task",
 	ERROR_CODE_100032: "failed to delete tasks from the source queue",
 	ERROR_CODE_100033: "failed to parse task payload into json",
